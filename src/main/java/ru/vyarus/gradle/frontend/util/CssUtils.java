46a7e2479c06a4a8f18117cc62c186118a209726
Vyacheslav Rusakov <xvik@noveogroup.com>
2023-02-28 15:35:59 +0700

    use regexp instead of pg-css to find css links

--- src/main/java/ru/vyarus/gradle/frontend/util/CssUtils.java
+++ src/main/java/ru/vyarus/gradle/frontend/util/CssUtils.java
@@ -1,17 +1,12 @@
 package ru.vyarus.gradle.frontend.util;
 
-import com.helger.css.ECSSVersion;
-import com.helger.css.decl.CascadingStyleSheet;
-import com.helger.css.decl.visit.AbstractModifyingCSSUrlVisitor;
-import com.helger.css.decl.visit.CSSVisitor;
-import com.helger.css.reader.CSSReader;
-import com.helger.css.utils.CSSDataURLHelper;
-
-import javax.annotation.Nonnull;
 import java.io.File;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Vyacheslav Rusakov
@@ -19,26 +14,32 @@ import java.util.List;
  */
 public final class CssUtils {
 
+    public static final Pattern URL_PATTERN = Pattern.compile(
+            "url\\s*\\(\\s*['\"]?(?<url>[^'\")]*)\\s*['\"]?", Pattern.DOTALL);
+
     private CssUtils() {
     }
 
     public static List<String> findLinks(final File file) {
-        final CascadingStyleSheet css = CSSReader.readFromFile(file, StandardCharsets.UTF_8, ECSSVersion.CSS30);
-        if (css == null) {
-            throw new IllegalStateException("Failed to read css file: " + file.getAbsolutePath());
+        String content;
+        try {
+            content = Files.readString(file.toPath());
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to read file: " + file.getAbsolutePath(), e);
         }
-        final List<String> urls = new ArrayList<>();
-        CSSVisitor.visitCSSUrl(css, new AbstractModifyingCSSUrlVisitor() {
-            @Nonnull
-            @Override
-            protected String getModifiedURI(@Nonnull String url) {
-                // ignore data urls and svg references
-                if (!CSSDataURLHelper.isDataURL(url) && !url.startsWith("#")) {
-                    urls.add(url);
-                }
-                return url;
+        return findLinks(content);
+    }
+
+    public static List<String> findLinks(final String content) {
+        final List<String> res = new ArrayList<>();
+        final Matcher matcher = URL_PATTERN.matcher(content);
+        while (matcher.find()) {
+            String url = matcher.group("url");
+            if (!url.startsWith("data:")) {
+                res.add(url.trim());
             }
-        });
-        return urls;
+        }
+
+        return res;
     }
 }
---
+++ src/test/groovy/ru/vyarus/gradle/frontend/util/CssUtilsTest.groovy
@@ -0,0 +1,46 @@
+package ru.vyarus.gradle.frontend.util
+
+import spock.lang.Specification
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 28.02.2023
+ */
+class CssUtilsTest extends Specification {
+
+    def "Check url recognition"() {
+
+        expect:
+        CssUtils.findLinks(cont as String) == [res]
+
+        where:
+        cont                                       | res
+        "background-image: url('test/test.gif');"  | 'test/test.gif'
+        'background: url("test2/test2.gif");'      | 'test2/test2.gif'
+        'background-image: url(test3/test3.gif);'  | 'test3/test3.gif'
+        'background: url   ( test4/ test4.gif );'  | 'test4/ test4.gif'
+        'background: url( " test5/test5.gif"   );' | 'test5/test5.gif'
+
+    }
+
+    def "Check data urls ignores"() {
+
+        expect:
+        CssUtils.findLinks("--bs-navbar-toggler-icon-bg: url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e\");")
+            == []
+    }
+
+    def "Check css urls extraction"() {
+
+        when: 'searching for links'
+        def res = CssUtils.findLinks(new File('src/test/resources/links/materialdesignicons.css'))
+        then: 'links found'
+        res.size() == 6
+        res == ['../fonts/materialdesignicons-webfont.eot?v=2.5.94',
+                '../fonts/materialdesignicons-webfont.eot?#iefix&v=2.5.94',
+                '../fonts/materialdesignicons-webfont.woff2?v=2.5.94',
+                '../fonts/materialdesignicons-webfont.woff?v=2.5.94',
+                '../fonts/materialdesignicons-webfont.ttf?v=2.5.94',
+                '../fonts/materialdesignicons-webfont.svg?v=2.5.94#materialdesigniconsregular']
+    }
+}

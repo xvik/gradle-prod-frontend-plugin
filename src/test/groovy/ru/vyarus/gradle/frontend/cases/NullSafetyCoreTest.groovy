c70da714ae4a60b94fbad9ece30591b134990bb8
Vyacheslav Rusakov <xvik@noveogroup.com>
2023-07-10 16:03:51 +0700

    test null safety for options

---
+++ src/test/groovy/ru/vyarus/gradle/frontend/cases/NullSafetyCoreTest.groovy
@@ -0,0 +1,62 @@
+package ru.vyarus.gradle.frontend.cases
+
+import ru.vyarus.gradle.frontend.core.info.resources.HtmlInfo
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 10.07.2023
+ */
+class NullSafetyCoreTest extends AbstractCoreTest {
+
+    def "Check null safety"() {
+
+        fileFromClasspath('webapp/index.html', '/cases/bootstrap.html')
+
+        when: "processing bootstrap application"
+        def res = run(builder('webapp').
+                downloadResources(null)
+                .preferMinDownload(null)
+                .downloadSourceMaps(null)
+                .minifyCss(null)
+                .minifyJs(null)
+                .minifyHtml(null)
+                .minifyHtmlCss(null)
+                .minifyHtmlJs(null)
+                .applyAntiCache(null)
+                .applyIntegrity(null)
+                .htmlExtensions(null)
+                .generateSourceMaps(null)
+                .gzip(null))
+
+        then: "optimization done"
+        res.getHtmls().size() == 1
+        HtmlInfo html = res.getHtmls()[0]
+        html.changes == ['changed links', 'minified']
+        html.js.size() == 1
+        html.pureHtml
+        html.parsedDocument != null
+        with(html.js[0]) {
+            remote
+            changes.size() == 5
+            changes.containsAll(['crossorigin removed', 'integrity removed', 'integrity token applied'])
+            target.startsWith("js/bootstrap.bundle.min.js?")
+            element.attr('integrity').length() > 0
+            element.attr('crossorigin').length() == 0
+            gzip != null
+            file != null
+            sourceMap != null
+        }
+        html.css.size() == 1
+        with(html.css[0]) {
+            remote
+            changes.size() == 5
+            changes.containsAll(['crossorigin removed', 'integrity removed', 'integrity token applied'])
+            target.startsWith("css/bootstrap.min.css?")
+            element.attr('integrity').length() > 0
+            element.attr('crossorigin').length() == 0
+            gzip != null
+            file != null
+            sourceMap != null
+        }
+    }
+}

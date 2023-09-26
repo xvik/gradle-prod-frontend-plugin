290a919265c323e7148030e193420696e5451646
Vyacheslav Rusakov <xvik@noveogroup.com>
2023-09-26 14:30:46 +0700

    fix test for linux

--- src/test/groovy/ru/vyarus/gradle/frontend/util/FileUtilsTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/frontend/util/FileUtilsTest.groovy
@@ -78,7 +78,11 @@ class FileUtilsTest extends AbstractTest {
         FileUtils.relative(testProjectDir, file('js.js')) == 'js.js'
         FileUtils.relative(file('index.html'), file('sub/js.js')) == 'sub/js.js'
         FileUtils.relative(file('sub/index.html'), file('js.js')) == '../js.js'
-        FileUtils.relative(file('sub\\index.html'), file('js.js')) == '../js.js'
+
+        if (isWin) {
+            // on linux it would be single file name
+            FileUtils.relative(file('sub\\index.html'), file('js.js')) == '../js.js'
+        }
     }
 
     def "Check file read-write"() {
